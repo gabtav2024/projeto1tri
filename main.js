fb6e8c3965292d0eb90213000e9852e206ebbb70
gabtav2024 <163063238+gabtav2024@users.noreply.github.com>
2024-05-03 18:13:20 -0300

    js

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-31T23:59:59");
+
 const tempoObjetivo2 = new Date("2024-12-31T23:59:59");
+
 const tempoObjetivo3 = new Date("2024-12-31T23:59:59");
+
 const tempoObjetivo4 = new Date("2024-12-31T23:59:59");
 
 
